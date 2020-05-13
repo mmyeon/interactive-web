36b1b5c7d4e796863362b64c5f5c8f944097bab5
Meagle <mindfulyeon@gmail.com>
2020-05-13 10:52:23 +0900

    [상단 진행바] JS :  스크롤에 따라 width 다르게 적용

--- practice/3D_scroll_practice/JS/wall3D.js
+++ practice/3D_scroll_practice/JS/wall3D.js
@@ -3,6 +3,7 @@
   const houseElem = document.querySelector(".house");
   // 중복되는 부분이라 따로 빼기
   // let maxScrollValue = document.body.offsetHeight - this.window.innerHeight;
+  const barElem = document.querySelector(".progress-bar");
   let maxScrollValue;
 
   // 윈도우 창사이즈가 바뀌면 maxScrollValue가 갱신하도록 세팅함.
@@ -23,8 +24,14 @@
     // maxScrollValue가 0 ~ 1 로 너무 작은 값이라 1000을 곱함 ( 0 ~ 1000 으로 세팅)
     // 하우스자체가 default로 -490이니까 여기에도 적용. 적용하지 않으면 스트롤시작시 바로 0부터 시작해 다음 벽면이 화면에 가득참.
     // 스크롤 범위를 1000에서 950으로 줄여서 마지막에 스크롤이 덜 되게 설정. 그 비율만큼 스크롤 되는 양이 줄도록
-    const zMove = (pageYOffset / maxScrollValue) * 980 - 490;
+
+    // 스크롤 퍼센티지
+    const scrollPer = pageYOffset / maxScrollValue;
+    const zMove = scrollPer * 980 - 490;
     houseElem.style.transform = "translateZ( " + zMove + "vw)";
+
+    // progress bar
+    barElem.style.width = scrollPer * 100 + "%";
   });
 
   // 윈도우 객체에 리사이즈 이벤트 걸어줌
