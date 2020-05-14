0f0ebbea223499a4833bdc7c3c1e6b35eeb26c12
Meagle <mindfulyeon@gmail.com>
2020-05-14 17:42:11 +0900

    키보드 누르는동안 좌우로 이동하도록 설정 : requestAnimationFrame

--- practice/3D_scroll_practice/JS/characters.js
+++ practice/3D_scroll_practice/JS/characters.js
@@ -34,6 +34,7 @@ function Character(info) {
   document.querySelector(".stage").appendChild(this.mainElem);
 
   //   매개변수로 전달된 객체 잘 받아오는지 확인
+  //  info 정보를 담은 객체
   //   console.log(info);
   //    캐릭터의 left값으로 클릭된 위치를 넣음
   this.mainElem.style.left = info.xPos + "%";
@@ -41,6 +42,11 @@ function Character(info) {
   this.scrollState = false;
   // 바로 이전(마지막) 스크롤 위치
   this.lastScrollTop = 0;
+  // xPos를 객체의 속성으로도 등록해줌. 접근할 수 있도록.
+  this.xPos = info.xPos;
+  this.speed = 0.3;
+  // 움직임 방향 알려주는 속성 추가
+  this.direction;
   this.init();
   //   생성자로 생성한 인스턴스 객체가 공통으로 사용하는 속성은 프로토타입 객체에 만든다.
 }
@@ -70,7 +76,7 @@ Character.prototype = {
       // 조건문에서 값을 가지고 있으면 true, 없으면 false
       if (!self.scrollState) {
         self.mainElem.classList.add("running");
-        // console.log("runni ng 클래스 붙었음");
+        // console.log("running 클래스 붙었음");
       }
 
       // 0.5초 뒤에 함수 실행, setTimeout은 숫자를 리턴함
@@ -98,6 +104,11 @@ Character.prototype = {
     });
 
     window.addEventListener("keydown", function (e) {
+      // 키다운이벤트가 얼마나 발생하는지 체크
+      // 애니메이션이 자연스러우려면 최소 24 ~ 30프레임이 되어야 하는데
+      // 키다운은 초당 10프레임이기에 움직임이 버벅이고 끊기고 부자연스러운 것
+      // 키다운이 아닌 requestAnimationFrame 활용해야함
+      // console.log("키다운");
       // self.lastScrollLeft = this.pageXOffset;
       // console.log(pageXoffset);
       // console.log(self.lastScrollLeft);
@@ -105,18 +116,42 @@ Character.prototype = {
       // console.log(e.keyCode);
       if (e.keyCode == 37) {
         // 왼쪽
+        self.direction = "left";
         self.mainElem.setAttribute("data-direction", "left");
         // 걷는 움직임 추가
         self.mainElem.classList.add("running");
+        self.run(self);
       } else if (e.keyCode == 39) {
         // 오른쪽
+        self.direction = "right";
         self.mainElem.setAttribute("data-direction", "right");
         // 걷는 움직임 추가
         self.mainElem.classList.add("running");
+        self.run(self);
       }
     });
     window.addEventListener("keyup", function (e) {
       self.mainElem.classList.remove("running");
+    });
+  },
+  run: function (self) {
+    // 캐릭터가 이동하도록 설정
+
+    if (self.direction == "left") {
+      self.xPos -= self.speed;
+    } else if (self.direction == "right") {
+      self.xPos += self.speed;
+    }
+
+    // self 오류나서 확인하기위해 찍어봄
+    // requestAnimaionFrame으로 컨텍스트(실행되는 문맥)이 바뀌어서 this가 변경됨
+    // console.log(self);
+    // 바뀐 속성 값을 CSS 스타일에도 적용
+    self.mainElem.style.left = self.xPos + "%";
+
+    // 반복시키기
+    requestAnimationFrame(function () {
+      self.run(self);
     });
   },
 };
