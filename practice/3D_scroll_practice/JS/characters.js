4beed54b46cf911347aa47299fb4222435d6dfce
Meagle <mindfulyeon@gmail.com>
2020-05-14 14:45:56 +0900

    스크롤 이벤트가 발생하면 팔, 다리가 움직이는 스타일 적용

--- practice/3D_scroll_practice/JS/characters.js
+++ practice/3D_scroll_practice/JS/characters.js
@@ -37,4 +37,28 @@ function Character(info) {
   //   console.log(info);
   //    캐릭터의 left값으로 클릭된 위치를 넣음
   this.mainElem.style.left = info.xPos + "%";
+  this.init();
+  //   생성자로 생성한 인스턴스 객체가 공통으로 사용하는 속성은 프로토타입 객체에 만든다.
 }
+
+// 원래 존재하는 프로토타입에 메서드 추가.
+// Character.prototype.xxxxx = function () {
+
+// };
+
+// 프로토타입객체를 빈 객체로 재정의하려고 하기 떄문에 원래 갖고 있던 constructor 속상을 다시 만들어줘야함.
+Character.prototype = {
+  constructor: Character,
+  // 메서드 추가, 캐릭터 만들어지는 순간에 실행되어야 함
+  init: function () {
+    // 여기서 this는 캐릭터 객체를 가리킴
+    // console.log(this);
+    const self = this;
+    // 이벤트핸들러에서 this는 이벤트리스너를 실행한 객체. window
+    window.addEventListener("scroll", function () {
+      // console.log(this);
+      // 우리가 원하는 this는 캐릭터생성자로 생성해낸 인스턴스 객체
+      self.mainElem.classList.add("running");
+    });
+  },
+};
